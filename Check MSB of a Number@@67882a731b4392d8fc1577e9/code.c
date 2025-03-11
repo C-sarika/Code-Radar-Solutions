e7811141f21eda7d300e8925d100a0528664174c
C-sarika <csarika352@gmail.com>
2025-03-11 23:02:02 +0530

    Solution to Check MSB of a Number

--- Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
+++ Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
@@ -1,26 +1,20 @@
 // Your code here...
 #include <stdio.h>
-#include <stdio.h>
-
-void checkMSB(int num) {
-    int msb = 1 << 31;  // MSB mask for 32-bit integer
-
-    if (num & msb)
-        printf("MSB is set (1)\n");
-    else
-        printf("MSB is not set (0)\n");
-}
 
 int main() {
     int num;
-    
-    // User input
+
+
     printf("Enter a number: ");
     scanf("%d", &num);
 
-    // Check MSB
-    checkMSB(num);
+    
+    if (num < 0)  
+        printf("MSB is set (1)\n");
+    else
+        printf("MSB is not set (0)\n");
 
     return 0;
 }
+
 
